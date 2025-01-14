3e9b208977d40f8259580b56fc5a01e054b0e35e
GuillemSoto <guillem.soto.7e8@itb.cat>
2025-01-14 12:17:50 +0100

    commit

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
@@ -1,17 +1,39 @@
 package cat.itb.m78.exercices
 
 
+import HelloWorldApp
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import cat.itb.m78.exercices.theme.AppTheme
+import io.ktor.client.request.forms.*
+import m78exercices.composeapp.generated.resources.Res
+import m78exercices.composeapp.generated.resources.myImage
 import org.jetbrains.compose.reload.DevelopmentEntryPoint
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
+
 internal fun App() = AppTheme {
-    Box(Modifier.fillMaxSize()){
-        Text("Your app goes here!", Modifier.align(Alignment.Center))
+    Box(modifier = Modifier.fillMaxSize()){
+        Column(){
+            Text("This is a resource string")
+            Image(
+                painter = painterResource(Res.drawable.myImage),
+                modifier = Modifier.size(100.dp),
+                contentDescription = null
+            )
+        }
     }
+
 }
